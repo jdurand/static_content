9a64628bcff8c969093591e1773de7cd9ae034bd
Pedro Nascimento <pnascimento@gmail.com>
2012-07-05 15:10:11 -0300

    Content can be created or can already exist.

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,11 +1,20 @@
 class Content < ActiveRecord::Base
-  attr_accessible :text, as: :admin
-  validates :name, :slug, :text, presence: true
+  attr_accessible :slug, :text, as: :admin
+  validates :slug, presence: true
   def self.markdown(slug)
     binding
     RDiscount.new(content(slug).text).to_html.html_safe
   rescue
     nil
   end
-
+  class << self
+    def from_slug(slug, options={})
+      find_or_initialize_by_slug(slug).tap do |content|
+        if content.new_record?
+          content.text = options[:default] if content.new_record?
+          content.save
+        end
+      end
+    end
+  end
 end
--- spec/models/content_spec.rb
+++ spec/models/content_spec.rb
@@ -1,7 +1,31 @@
 require 'spec_helper'
 describe Content do
   it { should allow_mass_assignment_of(:text).as(:admin) }
-  [:name, :slug, :text].each do |attribute|
+  [:slug].each do |attribute|
     it { should validate_presence_of(attribute) }
+  end
+  context "given a new record" do
+    it 'creates a content based on a slug' do
+      expect{Content.from_slug(:my_awesome_slug)}.to change(Content, :count).by(1)
+    end
+    it 'creates a content with a default text' do
+      default_text = "Hi, this is dog"
+      Content.from_slug(:my_awesome_slug, default: default_text).text.should == default_text
+    end
+  end
+
+  context "given an already created content" do
+    before do
+      @content = Content.create({slug: :my_already_created_slug, text: "Hi, this is dog"}, as: :admin)
+    end
+    it "finds an already content with a slug" do
+      fetched_content = Content.from_slug(:my_already_created_slug, default: "I have no idea what I'm doing")
+      fetched_content.should == @content
+    end
+
+    it "doesnt touch it" do
+      fetched_content = Content.from_slug(:my_already_created_slug, default: "I have no idea what I'm doing")
+      fetched_content.text.should == "Hi, this is dog"
+    end
   end
 end
