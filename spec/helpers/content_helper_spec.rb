8b24d0c75aab20997f262f40dcab108527a369b2
Pedro Nascimento <pnascimento@gmail.com>
2012-08-23 16:18:43 -0300

    Organizing specs for our helpers.

--- spec/helpers/content_helper_spec.rb
+++ spec/helpers/content_helper_spec.rb
@@ -5,16 +5,24 @@ describe ContentHelper do
     content = Content.new({slug: :sidebar_text, text: "Hi, this is dog"}, as: :admin)
     Content.should_receive(:from_slug).with(@options).and_return(content)
   end
-  it "returns a content by slug" do
-    helper.content(@options).should == "<p>Hi, this is dog</p>\n"
+
+  context "parsed content" do
+    it "returns a content by slug" do
+      helper.content(@options).should == "<p>Hi, this is dog</p>\n"
+    end
+
+    it "is aliased as c" do
+      helper.c(@options).should == "<p>Hi, this is dog</p>\n"
+    end
   end
-  it "should have an alias as c" do
-    helper.c(@options).should == "<p>Hi, this is dog</p>\n"
-  end
-  it "returns a raw content by slug" do
-    helper.raw_content(@options).should == "Hi, this is dog"
-  end
-  it "should have an alias as rc" do
-    helper.rc(@options).should == "Hi, this is dog"
+
+  context "raw content" do
+    it "returns a raw content by slug" do
+      helper.raw_content(@options).should == "Hi, this is dog"
+    end
+
+    it "is aliased as rc" do
+      helper.rc(@options).should == "Hi, this is dog"
+    end
   end
 end
