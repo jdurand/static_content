96bc5029e4919bfca241fb06bad2376ea9afa91f
Mauro George <maurogot@gmail.com>
2013-11-08 16:01:29 -0200

    Update Dummy app to rails 4 to avoid warnings

--- spec/dummy/config/initializers/secret_token.rb
+++ spec/dummy/config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Dummy::Application.config.secret_token = '697e0d972d7f6118fd96fc17c52d0dfedf824efa1d32dd96e0e2eb09a2d80683a64ac895aeecfc09404513d6d64be1d6bf16380cf4ccca2fb6c12f95c77b7e23'
+Dummy::Application.config.secret_key_base = 'A8d2Uofdj6tKdSOJ1O5CkLW5vxs7Ikn9ogiEDIDeoxHXLA8Yd3hoyCwJ5AL8FWCUJHaoJO1atzRmwsNPKkYBBALDONjDHgRe3UUKyg3yrqGYEI2k0rSdCGt71ab41'
